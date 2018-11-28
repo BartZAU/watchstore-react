2a53f72ab05e8a08ed4bd6ad29c5e019d9ca8db5
Bart Zabielski <zabielski@protonmail.com>
2018-11-28 20:14:12 +1100

    carousel image position css added

--- watchstore-react-redux/src/components/MainCarousel.js
+++ watchstore-react-redux/src/components/MainCarousel.js
@@ -7,22 +7,19 @@ export default class MainCarousel extends Component {
   render() {
     return (
       <Carousel className="carousel-wrapper">
-        <Carousel.Item>
-          <img height={30} src="/img/carousel-img-1.jpeg" />
+        <Carousel.Item id="item-1">
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img height={30} src="/img/carousel-img-2.jpeg" />
+        <Carousel.Item id="item-1">
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img height={30} src="/img/carousel-img-3.jpg" />
+        <Carousel.Item id="item-3">
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
