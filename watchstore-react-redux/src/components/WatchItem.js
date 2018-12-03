f4672109900e43628d16a8c4363ab78826906381
Bart Zabielski <zabielski@protonmail.com>
2018-12-03 23:39:00 +1100

    added to cart item state added

--- watchstore-react-redux/src/components/WatchItem.js
+++ watchstore-react-redux/src/components/WatchItem.js
@@ -4,11 +4,17 @@ import { Col, Thumbnail, Button } from "react-bootstrap";
 import "./WatchItem.css";
 
 class WatchItem extends React.Component {
+  state = {
+    isInCart: false
+  };
+
   handleClick = e => {
     this.props.addCartItem(this.props.name, this.props.price);
+    this.setState({ isInCart: true });
   };
 
   render() {
+    // console.log(this.props);
     // console.log(this.props);
     return (
       <Col xs={6} md={4}>
@@ -22,11 +28,13 @@ class WatchItem extends React.Component {
             <p>{this.props.description}</p>
             <p>{`$${this.props.price.toFixed(2)}`}</p>
           </div>
-          <img
-            className="added-to-cart"
-            src="./img/products/cart.svg"
-            alt="cart"
-          />
+          {this.state.isInCart ? (
+            <img
+              className="added-to-cart"
+              src="./img/products/cart.svg"
+              alt="cart"
+            />
+          ) : null}
           <Button onClick={this.handleClick} bsStyle="default">
             Add to cart
           </Button>
