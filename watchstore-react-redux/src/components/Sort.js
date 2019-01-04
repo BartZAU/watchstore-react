a1466270f72c8602bba4a91450c2bcda356043c5
Bart Zabielski <zabielski@protonmail.com>
2019-01-05 07:48:36 +1100

    arrows added to buttons only styling

--- watchstore-react-redux/src/components/Sort.js
+++ watchstore-react-redux/src/components/Sort.js
@@ -5,16 +5,18 @@ import { Button } from "react-bootstrap";
 
 import styles from "./Sort.module.css";
 
+// icon class name to be passed from watchList as a prop to sort
+// arrow toggle - maybe just have initial state false then add a toggle for true/false for item sort asc/sce
 const Sort = ({ sortKey, children, onSort }) => (
   <Button onClick={() => onSort(sortKey)} className={styles.sort_button}>
     {children}
     <i
       style={{
-        fontSize: "14px",
-        paddingLeft: "2.5px",
-        paddingRight: "2.5px"
+        fontSize: "13px",
+        paddingLeft: "4px"
+        // paddingRight: "2px"
       }}
-      className="fas fa-shopping-cart "
+      className="fas fa-angle-double-up"
     />
   </Button>
 );
