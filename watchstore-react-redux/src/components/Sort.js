01a95f079d9d844bd540dc0804c3ef0f0c254514
Bart Zabielski <zabielski@protonmail.com>
2019-01-05 11:01:08 +1100

    sort buttons finished

--- watchstore-react-redux/src/components/Sort.js
+++ watchstore-react-redux/src/components/Sort.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { sortBy } from "lodash";
 import { Button } from "react-bootstrap";
@@ -7,18 +7,52 @@ import styles from "./Sort.module.css";
 
 // icon class name to be passed from watchList as a prop to sort
 // arrow toggle - maybe just have initial state false then add a toggle for true/false for item sort asc/sce
-const Sort = ({ sortKey, children, onSort }) => (
-  <Button onClick={() => onSort(sortKey)} className={styles.sort_button}>
-    {children}
-    <i
-      style={{
-        fontSize: "13px",
-        paddingLeft: "4px"
-        // paddingRight: "2px"
-      }}
-      className="fas fa-angle-double-up"
-    />
-  </Button>
-);
+
+export class Sort extends Component {
+  state = {
+    sortToggle: false
+  };
+
+  handleSortClick = () => {
+    this.setState({ sortToggle: !this.state.sortToggle });
+
+    this.props.onSort(this.props.sortKey);
+  };
+
+  render() {
+    const { children } = this.props;
+    const upIcon = (
+      <i
+        style={{
+          fontSize: "13px",
+          paddingLeft: "4px"
+        }}
+        className="fas fa-angle-double-up"
+      />
+    );
+
+    const downIcon = (
+      <i
+        style={{
+          fontSize: "13px",
+          paddingLeft: "4px"
+        }}
+        className="fas fa-angle-double-down"
+      />
+    );
+
+    const icon = this.state.sortToggle ? downIcon : upIcon;
+
+    return (
+      <Button
+        onClick={this.handleSortClick}
+        bsSize="xsmall"
+        className={styles.sort_button}
+      >
+        {children} {icon}
+      </Button>
+    );
+  }
+}
 
 export default Sort;
